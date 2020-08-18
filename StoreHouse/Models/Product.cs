7d16753ac905d96a34ce64f4c39c7e05d87639e0
Omarlin <omarlinneftali1997@gmail.com>
2020-08-18 13:47:38 -0400

    proyecto

--- StoreHouse/Models/Product.cs
+++ StoreHouse/Models/Product.cs
@@ -10,16 +10,22 @@ namespace StoreHouse.Models
     public class Product
     {
         public int ProductID { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
 
         public string Description { get; set; }
+        [Required]
 
         public double Price { get; set; }
+        [Required]
 
         public int Stock { get; set; }
+        [Required]
 
         [Display(Name = "Categoria")]
         public int CategoryID { get; set; }
+        [Required]
 
         public Category Category { get; set; }
     }
